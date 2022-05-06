b92690d30329d713804e11fd14e62c1732bbd11f
frulx241 <104719989+frulx241@users.noreply.github.com>
2022-05-06 18:50:50 +0200

    Design changes

--- html/script.js
+++ html/script.js
@@ -2,55 +2,51 @@ var number0Length = 1;
 var numberNLength = 1;
 var letterLength = 1;
 
-
 function generateFN() {
-  let flightnumber = "";
-  var numbers0 = "123456789";
-  var numbersN = "0123456789";
-  var letters = "ABCDEFGHJKLMNPQRSTUVWXYZ";
+    let flightnumber = "";
+    var numbers0 = "123456789";
+    var numbersN = "0123456789";
+    var letters = "ABCDEFGHJKLMNPQRSTUVWXYZ";
 
-  if (document.getElementById('style0').checked) {
-    number0Length = 1;
-    numberNLength = 0;
-    letterLength = 1;
-  }
-  else if (document.getElementById('style1').checked) {
-    number0Length = 1;
-    numberNLength = 0;
-    letterLength = 2;
-  }
-  else if (document.getElementById('style2').checked) {
-    number0Length = 1;
-    numberNLength = 1;
-    letterLength = 1;
-  }
-  else if (document.getElementById('style3').checked) {
-    number0Length = 1;
-    numberNLength = 1;
-    letterLength = 2;
-  }
-  else if (document.getElementById('style4').checked) {
-    number0Length = 1;
-    numberNLength = 2;
-    letterLength = 1;
-  }
-  else {
-    number0Length = 1;
-    numberNLength = 3;
-    letterLength = 0;
-  }
+    if (document.getElementById("style0").checked) {
+        number0Length = 1;
+        numberNLength = 0;
+        letterLength = 1;
+    } else if (document.getElementById("style1").checked) {
+        number0Length = 1;
+        numberNLength = 0;
+        letterLength = 2;
+    } else if (document.getElementById("style2").checked) {
+        number0Length = 1;
+        numberNLength = 1;
+        letterLength = 1;
+    } else if (document.getElementById("style3").checked) {
+        number0Length = 1;
+        numberNLength = 1;
+        letterLength = 2;
+    } else if (document.getElementById("style4").checked) {
+        number0Length = 1;
+        numberNLength = 2;
+        letterLength = 1;
+    } else if (document.getElementById("style5").checked) {
+        number0Length = 1;
+        numberNLength = 3;
+        letterLength = 0;
+    }
 
-  for (var i = 0; i < number0Length; i++) {
-    var rN0 = Math.floor(Math.random() * numbers0.length);
-    flightnumber += numbers0.substring(rN0, rN0 +1);
-  }
-  for (var j = 0; j < numberNLength; j++){
-    flightnumber += numbersN.charAt(Math.floor(Math.random() * numbersN.length));
-  }
-  for (var k = 0; k < letterLength; k++){
-    flightnumber += letters.charAt(Math.floor(Math.random() * letters.length));
-  }
-  document.getElementById('flightnumberBox').value = flightnumber;
+    for (var i = 0; i < number0Length; i++) {
+        var rN0 = Math.floor(Math.random() * numbers0.length);
+        flightnumber += numbers0.substring(rN0, rN0 + 1);
+    }
+    for (var j = 0; j < numberNLength; j++) {
+        flightnumber += numbersN.charAt(
+            Math.floor(Math.random() * numbersN.length)
+        );
+    }
+    for (var k = 0; k < letterLength; k++) {
+        flightnumber += letters.charAt(
+            Math.floor(Math.random() * letters.length)
+        );
+    }
+    document.getElementById("flightnumberBox").value = flightnumber;
 }
-
-
